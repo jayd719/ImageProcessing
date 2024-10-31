b2e1c092b98cc9d70016e4fa100d88bf813b67b0
JD-MBP <jashansingh29@gmail.com>
2024-10-31 17:01:17 +0530

    k

--- Assignment3/Code/a3.py
+++ Assignment3/Code/a3.py
@@ -10,10 +10,14 @@ __updated__ = "2024-10-30"
 """
 
 from os import path, makedirs
-from cv2 import imread, imwrite
+import cv2 as cv
+
+# Contants
+OUTPUT = "Output_images"
+EDGE_MAPS = "Edge_maps"
 
 
-def output_image(filename, img):
+def output_image(filename, folder, img):
     """
     -------------------------------------------------------
     Saves the provided image to the 'output' directory.
@@ -28,14 +32,28 @@ def output_image(filename, img):
     -------------------------------------------------------
     """
     # Path for Cross Platfrom support
-    output_dir = path.join(
-        path.dirname(__file__).replace("Code", ""), "Output_Images"
-    )
+    output_dir = path.join(path.dirname(__file__).replace("Code", ""), folder)
     if not path.exists(output_dir):
         makedirs(output_dir)
     # Path to save the image
     path_ = path.join(output_dir, filename)
 
     # Save Image using Python's built in function
-    imwrite(path_, img)
+    cv.imwrite(path_, img)
     return None
+
+
+def SIFT(img):
+    # convert the image into grayscale.
+    gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    threshold1 = 100
+    threshold2 = 200
+    edge_map = cv.Canny(img, threshold1, threshold2)
+
+    output_image("file.tif", EDGE_MAPS, edge_map)
+    # construct shift object
+    shift = cv.SIFT_create()
+    kp = shift.detect(gray_image, None)
+    img = cv.drawKeypoints(gray_image, kp, img)
+    return img
