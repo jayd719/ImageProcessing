d0e2d8a9a270fca3f1c5bb0905de0bdd3fdc2cec
JD-MBP <jashansingh29@gmail.com>
2024-11-19 14:36:13 -0500

    staring Assignment 4

---
+++ Assignment 4/Code/model1.py
@@ -0,0 +1,6 @@
+"""
+Task 1:  
+Freeze all layers of the pre-trained model, modify the last layer to have an output size of 10 
+(to match the number of classes in MNIST), train the model on the MNIST training set, and test 
+it on the MNIST test set.
+"""
